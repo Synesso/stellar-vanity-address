af42a8c831b26997ed3b04be3cebb5e018e68dc1
Jem Mawson <jem.mawson@gmail.com>
2019-08-19 11:07:38 +1000

    Init

---
+++ build.sbt
@@ -0,0 +1,6 @@
+scalaVersion := "2.12.6"
+name := "vanity-address"
+organization := "io.github.synesso"
+version := "1.0"
+
+libraryDependencies += "io.github.synesso" %% "scala-stellar-sdk" % "0.5.0"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,43 @@
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Balance, Flow, GraphDSL, Merge, Source}
+import akka.stream.{ActorMaterializer, FlowShape}
+import stellar.sdk.KeyPair
+
+object Main extends App {
+
+  implicit val sys: ActorSystem = ActorSystem("vanity-address")
+  implicit val mat: ActorMaterializer = ActorMaterializer()
+
+  def find(s: Seq[String]): Option[KeyPair] = {
+    val kp = KeyPair.random
+    val accId = kp.accountId
+    if (s.exists(accId.endsWith)) Some(kp) else None
+  }
+
+  val findFlow: Flow[Seq[String], KeyPair, NotUsed] =
+    Flow[Seq[String]].mapConcat[KeyPair](find(_).to[collection.immutable.Iterable])
+
+    Source.fromIterator[Seq[String]](() => Iterator.continually(args))
+      .via(balancer(findFlow, Runtime.getRuntime.availableProcessors()))
+      .runForeach(kp => println(kp.accountId + ":" + new String(kp.secretSeed)))
+
+
+  def balancer[In, Out](worker: Flow[In, Out, Any], workerCount: Int): Flow[In, Out, NotUsed] = {
+    import akka.stream.scaladsl.GraphDSL.Implicits._
+
+    Flow.fromGraph(GraphDSL.create() { implicit b =>
+      val balancer = b.add(Balance[In](workerCount, waitForAllDownstreams = true))
+      val merge = b.add(Merge[Out](workerCount))
+
+      for (_ <- 1 to workerCount) {
+        // for each worker, add an edge from the balancer to the worker, then wire it to the merge element
+        balancer ~> worker.async ~> merge
+      }
+
+      FlowShape(balancer.in, merge.out)
+    })
+  }
+
+}
